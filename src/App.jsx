f6c345a7e91c9aab31cbfcb6eaeac87bf2f1ca32
kazimehedihasan <kazimehedihasan458@.com>
2023-09-10 12:37:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App(){
+
+  return (
+    <>
+      <h1>load countries using</h1>
+    <div>
+
+    <Countries></Countries>
+    </div>
+        
+    </>
+  )
+}
+
+export default App
